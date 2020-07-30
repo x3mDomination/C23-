55d30b5c35f03a5c9c185b26111c4d0e04c850e5
x3mDomination <68398181+x3mDomination@users.noreply.github.com>
2020-07-30 14:00:11 -0400

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,19 @@
+class Ground {
+    constructor(x,y,w,h){
+        var options = {
+            isStatic:true
+        }
+        this.body = Bodies.rectangle(x,y,w,h,options);
+        this.width = w;
+        this.height = h;
+
+        World.add(world,this.body);
+    }
+
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill(255);
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
